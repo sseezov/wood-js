d1734ee584f6c695f49a93cf5eb75cbd85b5f3fb
sseezov <sseezov@mail.ru>
2026-04-25 19:12:20 +0700

    export redirect

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import { initListeners } from './handlers.js'
-import render from './render.js'
 import { h, Fragment } from './h.js'
+import { handlers, registerClick, registerSubmit, initListeners } from './handlers.js'
+import { render } from './render.js'
+import { redirect } from './router.js'
 import { initWood } from './initWood.js'
-import { handlers, registerClick, registerSubmit } from './handlers.js'
 
 export { h, Fragment }
 export { handlers, registerClick, registerSubmit, initListeners }
 export { render }
+export { redirect }
 export { initWood }
--- src/render.js
+++ src/render.js
@@ -1,4 +1,4 @@
-export default async function render (parentSelector, content) {
+export async function render (parentSelector, content) {
   const innerHTML = await content
   document.querySelector(parentSelector).innerHTML = innerHTML
 }
