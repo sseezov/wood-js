8744432cd0f0cb0734d2033bd0b49d95f584d989
sseezov <sseezov@mail.ru>
2026-04-26 12:53:47 +0700

    fix init function, route now mounts only if routes exists

--- src/initWood.js
+++ src/initWood.js
@@ -2,10 +2,14 @@ import { initListeners } from "./handlers"
 import { render } from "./render"
 import { mountRoute, setErrorComponent, setRoutes } from "./router"
 
-export function initWood(App, routes, errorComponent = { component: () => 'error', parentSelector: '#app' }) {
-  setRoutes(routes)
-  setErrorComponent(errorComponent)
+export function initWood(App, routes, errorComponent) {
+  if (routes) {
+    setRoutes(routes)
+    mountRoute()
+  }
+  if (errorComponent) {
+    setErrorComponent(errorComponent)
+  }
   render('#app', App())
   initListeners()
-  mountRoute()
 }
