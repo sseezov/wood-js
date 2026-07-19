876d48340a9bcb52d7ba559c925f8bc788aa6bf9
sseezov <sseezov@mail.ru>
2026-07-19 16:32:11 +0700

    restore index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { h, Fragment } from './h.js'
+import { render } from './render.js'
+import { redirect, navigateBack, refreshPage } from './router.js'
+import { initWood } from './initWood.js'
+
+export { h, Fragment }
+export { render }
+export { redirect, navigateBack, refreshPage }
+export { initWood }
